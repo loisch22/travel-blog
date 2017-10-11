6c9eba5578a2fac24400304772122d7f9ecec91f
Lois Choi <loisch22@gmail.com>
2017-10-10 17:01:31 -0700

    set models properties and relations

---
+++ TravelBlog/Models/Experience.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace TravelBlog.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Experience
+    {
+        [Key]
+        public int ExperienceId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int LocationId { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
---
+++ TravelBlog/Models/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace TravelBlog.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+        public string LocationCountry { get; set; }
+        public string LocationCity { get; set; }
+        public virtual ICollection<Experience> Experiences { get; set; }
+    }
+}
